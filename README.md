57e7864bfb06e6e786ae94438b91e90ee8c11459
Tyler Lane Howard <tug65404@temple.edu>
2020-03-23 14:52:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Results
+MultiLevelPerceptron Classifier results for NSL-KDD (with example parameters from scikit-learn documentation):
+
+Total datapoints: 22544
+
+Correct datapoints: 12820
+
+Mislabeled datapoints: 9724
+
+Percent correct: 56.87%
+
+
+## Other Parameters
+MultiLevelPerceptron Classifier results for NSL-KDD (with parameters from weka):
+
+Total datapoints: 22544
+
+Correct datapoints: 9711
+
+Mislabeled datapoints: 12833
+
+Percent correct: 43.08%
---
+++ sklearn-MLP-NSLKDD.py
@@ -0,0 +1,48 @@
+from sklearn import preprocessing
+from scipy.io.arff import loadarff
+from sklearn.neural_network import MLPClassifier
+import numpy as np
+
+
+KDDTrain, train_metadata = loadarff("KDDTrain+.arff")
+KDDTest, test_metadata = loadarff("KDDTest+.arff")
+
+training_nparray = np.asarray(KDDTrain.tolist())  # This is necessary to correctly shape the array
+testing_nparray = np.asarray(KDDTest.tolist())
+
+
+# Preprocess
+enc = preprocessing.OrdinalEncoder()
+
+encoded_dataset = enc.fit_transform(training_nparray)  # All categorical features are now numerical
+X_train = encoded_dataset[:, :-1]  # All rows, omit last column
+y_train = np.ravel(encoded_dataset[:, -1:])  # All rows, only the last column
+
+# Repeat preprocessing for test data
+encoded_dataset = enc.fit_transform(testing_nparray)
+X_test = encoded_dataset[:, :-1]
+y_test = np.ravel(encoded_dataset[:, -1:])
+
+
+# Weka arguments converted to scikit-learn
+# L -> learning_rate_init
+# H -> hidden_layer_size
+# M -> momentum
+# N -> max_iter
+# V -> validation_fraction
+# S -> random_state
+
+# Fit to model and predict
+clf = MLPClassifier(solver='sgd', alpha=1e-5,
+                    hidden_layer_sizes=(22,), random_state=0, learning_rate_init=.3,
+                    momentum=.2, max_iter=500, validation_fraction=0, )
+y_pred = clf.fit(X_train, y_train).predict(X_test)
+
+total_datapoints = X_test.shape[0]
+mislabeled_datapoints = (y_test != y_pred).sum()
+correct_datapoints = total_datapoints-mislabeled_datapoints
+percent_correct = (correct_datapoints / total_datapoints) * 100
+
+print("MultiLevelPerceptron Classifier results for NSL-KDD:\n")
+print("Total datapoints: %d\nCorrect datapoints: %d\nMislabeled datapoints: %d\nPercent correct: %.2f%%"
+      % (total_datapoints, correct_datapoints, mislabeled_datapoints, percent_correct))
